b858e61919ff91df3e943149ec0fd679354f9f13
Jing27540 <jhuang27540@gmail>
2024-01-30 12:44:42 -0500

    Added the created folders to repo.

---
+++ petti-management-system/src/test/java/com/petti2024/pettimanagementsystem/PettiManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petti2024.pettimanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettiManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
